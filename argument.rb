3c540d18f113a88fae028107fc7d91055003121b
Isabella Duran <2020ikd7@gmail.com>
2018-06-19 14:59:00 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -1,4 +1,4 @@
 def greeting(local_greeting,your_name,my_name,time_of_day)
-  puts "#{local_greeting} #{your_name}, I am #{my_name} and the time is #{time_of_day}"
+  puts "#{local_greeting} #{your_name}, I am #{my_name} and the time is #{time_of_day} you know what that means"
 end 
-greeting("hey", "Alyssa", "Isabella", "10:00am")
+greeting("hey", "Jamison", "Isabella", "10:00am")
