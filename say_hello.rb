6759dd777331d4f28a79975a726317ea6f4a22b7
Isabella Duran <2020ikd7@gmail.com>
2018-06-19 14:47:01 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,7 @@
-# Build your say_hello method here
+def morning_routine 
+   puts "Wake up when the alarm goes off and stay in bed"
+   puts "waits until my mom wakes me up"
+   puts "gets up and brushes my teeth"
+   puts "puts on cloths for today"
+end 
+morning_routine
