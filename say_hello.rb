4e7a529bd7bae058e36b4ad0484b182b25d2898a
Isabella Duran <2020ikd7@gmail.com>
2018-06-19 15:17:48 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,2 +1,3 @@
 def say_hello (greeting, best_friend_name, my_name)
-  Puts "#{greeting} #{best_friend_name}, have a great day, Love #{my_name}
+  Puts "#{greeting} #{best_friend_name}, have a great day, Love #{my_name}!!!"
+end
