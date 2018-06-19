8a548260544a55ce6664f331621d31d4da2cee07
Isabella Duran <2020ikd7@gmail.com>
2018-06-19 14:47:28 +0000

    automatically backed up by learn

--- say_hello.rb
+++
@@ -1,7 +0,0 @@
-def morning_routine 
-   puts "Wake up when the alarm goes off and stay in bed"
-   puts "waits until my mom wakes me up"
-   puts "gets up and brushes my teeth"
-   puts "puts on cloths for today"
-end 
-morning_routine
